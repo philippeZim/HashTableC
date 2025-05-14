44df0e54d6cdfb97f76f88ba4fc813348204495d
philippeZim <philippe.zim22@gmail.com>
2025-05-14 08:58:42 +0200

    .

--- final_hash.c
+++ final_hash.c
@@ -3,7 +3,8 @@
 #include "string.h"
 #include "stdlib.h"
 
-uint32_t hash_string(const char *s) {
+uint32_t hash_string(void *v_s) {
+    char *s = (char *) v_s;
     uint32_t h = 0;
     while (*s) {
         h = 31 * h + (unsigned char)(*s++);
@@ -11,19 +12,19 @@ uint32_t hash_string(const char *s) {
     return h;
 }
 
-uint32_t hash_int(const int a) {
-    return a;
+uint32_t hash_int(void *a) {
+    return *((int *)a);
 }
 
-uint32_t hash_float(const float a) {
+uint32_t hash_float(void *a) {
     uint32_t h;
-    memcpy(&h, &a, sizeof(float));
+    memcpy(&h, a, sizeof(float));
     return h;
 }
 
-uint32_t hash_double(const double a) {
+uint32_t hash_double(void *a) {
     uint64_t h;
-    memcpy(&h, &a, sizeof(double));
+    memcpy(&h, a, sizeof(double));
     return (uint32_t)(h ^ (h >> 32));
 }
 
@@ -56,6 +57,15 @@ uint32_t hashPerson(const Person *p) {
 
 */
 
+int equals_int(void *a, void *b) {
+    return *((int *)a) == *((int *)b);
+}
+
+int equals_string(void *a, void *b) {
+    int res = strcmp((char *)a, (char *)b);
+    return res == 0 ? 1 : 0;
+}
+
 typedef struct {
     void * key;
     void * val;
@@ -76,6 +86,7 @@ void map_init(Map *m, uint32_t (*custom_hash)(void *), int (*custom_equals)(void
     m->capa = 16;
     m->size = 0;
     m->custom_hash = custom_hash;
+    m->custom_equals = custom_equals;
     m->arr = calloc(m->capa, sizeof(Entry));
     if (!m->arr) {
         perror("error using malloc");
@@ -97,19 +108,27 @@ void map_add(Map *m, void *k, void *v) {
         }
         m->size = 0;
         for (int i = 0; i < m->capa >> 1; i++) {
-            map_add(m, arr_save[i].key, arr_save[i].val);
+            if (arr_save[i].in_use) {
+                map_add(m, arr_save[i].key, arr_save[i].val);
+            }
         }
         free(arr_save);
     }
     int hash = hash_spread(m->custom_hash(k));
     int index = hash & (m->capa - 1);
-    while (m->arr[index].in_use == 1) {
+    while (m->arr[index].in_use) {
+        if (m->custom_equals(m->arr[index].key, k)) {
+            m->size--;
+            break;
+        }
         index = (index + 1) % m->capa;
     }
     m->arr[index] = (Entry){k, v, 1};
+    
+    m->size++;
 }
 
-void *map_get(Map *m, void *k) {
+void *map_get(Map *m, void *k, void *default_val) {
     int hash = hash_spread(m->custom_hash(k));
     int index = hash & (m->capa - 1);
     for (int i = 0; i < m->capa; i++) {
@@ -118,5 +137,45 @@ void *map_get(Map *m, void *k) {
             return m->arr[cur_index].val;
         }
     }
-    return NULL;
+    return default_val;
+}
+
+
+const char *fruits[] = {
+    "pumpkin", "pumpkin", "banana", "melon", "melon", "banana", "banana",
+    "banana", "banana", "melon", "apple", "melon", "mango", "melon", "apple",
+    "melon", "mango", "apple", "banana", "mango", "pumpkin", "banana", "melon",
+    "pumpkin", "mango", "melon", "apple", "banana", "melon", "pumpkin", "pumpkin",
+    "apple", "pumpkin", "banana", "banana", "banana", "banana", "apple", "apple",
+    "apple", "pumpkin", "banana", "melon", "melon", "apple", "mango", "apple",
+    "melon", "pumpkin", "melon", "mango", "melon", "apple", "pumpkin", "mango",
+    "pumpkin", "melon", "apple", "banana", "melon", "pumpkin", "mango", "apple",
+    "melon", "pumpkin", "apple", "mango", "pumpkin", "mango", "apple", "apple",
+    "apple", "banana", "apple", "banana", "pumpkin", "mango", "mango", "melon",
+    "banana", "banana", "apple", "melon", "banana", "pumpkin", "melon", "apple",
+    "banana", "apple", "banana", "melon", "melon", "apple", "banana", "mango",
+    "pumpkin", "apple", "melon", "apple", "banana"
+};
+
+int main() {
+    Map test_map;
+    map_init(&test_map, hash_string, equals_string);
+    
+    for (int i = 0; i < 100; i++) {
+        int *default_val = calloc(1, sizeof(int));
+        if (!default_val) {
+            perror("error using malloc");
+            return 1;
+        }
+        int *cur_val = (int *)map_get(&test_map, (void *)fruits[i], (void *)default_val);
+        (*cur_val)++;
+        map_add(&test_map, (void *)fruits[i], (void *)cur_val);
+    }
+    printf("%d\n", test_map.size);
+    for (int i = 0; i < test_map.capa; i++) {
+        Entry e = test_map.arr[i];
+        if (e.in_use) {
+            printf("%s: %d\n", (char*)e.key, *(int*)e.val);
+        }
+    }
 }
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from itertools import combinations
+a = [
+    "pumpkin", "pumpkin", "banana", "melon", "melon", "banana", "banana",
+    "banana", "banana", "melon", "apple", "melon", "mango", "melon", "apple",
+    "melon", "mango", "apple", "banana", "mango", "pumpkin", "banana", "melon",
+    "pumpkin", "mango", "melon", "apple", "banana", "melon", "pumpkin", "pumpkin",
+    "apple", "pumpkin", "banana", "banana", "banana", "banana", "apple", "apple",
+    "apple", "pumpkin", "banana", "melon", "melon", "apple", "mango", "apple",
+    "melon", "pumpkin", "melon", "mango", "melon", "apple", "pumpkin", "mango",
+    "pumpkin", "melon", "apple", "banana", "melon", "pumpkin", "mango", "apple",
+    "melon", "pumpkin", "apple", "mango", "pumpkin", "mango", "apple", "apple",
+    "apple", "banana", "apple", "banana", "pumpkin", "mango", "mango", "melon",
+    "banana", "banana", "apple", "melon", "banana", "pumpkin", "melon", "apple",
+    "banana", "apple", "banana", "melon", "melon", "apple", "banana", "mango",
+    "pumpkin", "apple", "melon", "apple", "banana"
+]
 
-a = ["int", "float", "double", "char *", "void *"]
+d = dict()
 
-for i in range(len(a)):
-    for j in range(i, len(a)):
-        cur = f"typedef struct {{\n    {a[i]} key;\n    {a[j]} value;\n    int in_use;\n}} Entry;"
-        print(cur)
-        print()
+for x in a:
+    d[x] = d.get(x, 0) + 1
+print(len(d))
+for k, v in d.items():
+    print(k, v)
